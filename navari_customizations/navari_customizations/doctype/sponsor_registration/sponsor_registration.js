06ef42631aed6149255e7dfb64dc7c1de586fec2
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-06-11 19:11:14 +0300

    feat: add `Sponsor Registration` doctype

---
+++ navari_customizations/navari_customizations/doctype/sponsor_registration/sponsor_registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Navari Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sponsor Registration', {
+	// refresh: function(frm) {
+
+	// }
+});
